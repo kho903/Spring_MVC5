de9210b6689c69848546e541a02dbefd9f4a6a08
kho903 <gmldnr2222@naver.com>
2021-04-16 18:52:45 +0900

    RequestScope 빈 주입 - 자바

--- RequestScopeBeanJava/src/kr/co/softcampus/config/RootAppContext.java
+++ RequestScopeBeanJava/src/kr/co/softcampus/config/RootAppContext.java
@@ -1,9 +1,18 @@
 package kr.co.softcampus.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.RequestScope;
+
+import kr.co.softcampus.beans.DataBean1;
 
 // 프로젝트 작업 시 사용할 bean을 정의하는 클래스
 @Configuration
 public class RootAppContext {
 
+	@Bean
+	@RequestScope
+	public DataBean1 dataBean1() {
+		return new DataBean1();
+	}
 }
