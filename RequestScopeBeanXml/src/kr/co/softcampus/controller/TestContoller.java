4db877bb1625fa8914fdd151090c9e48a82cd669
kho903 <gmldnr2222@naver.com>
2021-04-16 19:14:58 +0900

    이름으로 주입 - XML

--- RequestScopeBeanXml/src/kr/co/softcampus/controller/TestContoller.java
+++ RequestScopeBeanXml/src/kr/co/softcampus/controller/TestContoller.java
@@ -1,5 +1,7 @@
 package kr.co.softcampus.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.softcampus.beans.DataBean1;
+import kr.co.softcampus.beans.DataBean2;
 
 @Controller
 public class TestContoller {
@@ -15,10 +18,17 @@ public class TestContoller {
 	@Lazy
 	DataBean1 requestBean1;
 
+	@Resource(name="requestBean2")
+	@Lazy
+	DataBean2 requestBean2;
+	
 	@GetMapping("/test1")
 	public String test1() {
 		requestBean1.setData1("문자열1");
 		requestBean1.setData2("문자열2");
+
+		requestBean2.setData3("문자열3");
+		requestBean2.setData4("문자열4");
 
 		return "forward:/result1";
 	}
@@ -28,8 +38,11 @@ public class TestContoller {
 
 		System.out.printf("requestBean1.data1 : %s\n", requestBean1.getData1());
 		System.out.printf("requestBean1.data2 : %s\n", requestBean1.getData2());
+		System.out.printf("requestBean2.data3 : %s\n", requestBean2.getData3());
+		System.out.printf("requestBean2.data4 : %s\n", requestBean2.getData4());
 
 		model.addAttribute("requestBean1", requestBean1);
+		
 		return "result1";
 	}
 }
